db158e677d4d75186056261d830b9595c0224324
Utkarsh Bhadauria <55799292+utk251199@users.noreply.github.com>
2020-01-15 15:26:35 +0530

    Update iris problem.py

--- iris problem.py
+++ iris problem.py
@@ -30,8 +30,29 @@ X_test = sc.transform(X_test)
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X_train,y_train)
+y_pred_random_forest = classifier.predict(X_test)
+cm_random_forest = confusion_matrix(y_test,y_pred_random_forest)
 
-y_pred = classifier.predict(X_test)
+from sklearn.svm import SVC
+classifier = SVC(random_state = 0)
+classifier.fit(X_train,y_train)
+y_pred_svm = classifier.predict(X_test)
+cm_svm = confusion_matrix(y_test,y_pred_svm)
 
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test,y_pred)
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train,y_train)
+y_pred_logistic = classifier.predict(X_test)
+cm_logistic = confusion_matrix(y_test,y_pred_logistic)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier()
+classifier.fit(X_train,y_train)
+y_pred_knn = classifier.predict(X_test)
+cm_knn = confusion_matrix(y_test,y_pred_knn)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train,y_train)
+y_pred_naive_bayes = classifier.predict(X_test)
+cm_naive_bayes = confusion_matrix(y_test,y_pred_naive_bayes)
