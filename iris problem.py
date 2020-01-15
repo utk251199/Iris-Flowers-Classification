b16bf0cb55f5f78cae330008f69920887200b572
Utkarsh Bhadauria <55799292+utk251199@users.noreply.github.com>
2020-01-15 14:54:19 +0530

    Add files via upload

---
+++ iris problem.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 15:37:51 2020
+
+@author: utkar
+"""
+
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Iris.csv')
+X = dataset.iloc[:,1:5].values
+y = dataset.iloc[:,5:6].values
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder = LabelEncoder()
+y = labelencoder.fit_transform(y)
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,train_size=0.67,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(X_train,y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
